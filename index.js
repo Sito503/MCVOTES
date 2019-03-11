7f11d6c7aa96300cf30596db9571b9bddfbb7826
Rajashow <rajashowparajuli@gmail.com>
2019-03-10 22:26:51 -0400

    add element to the tf questions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let answers = [];
-$(document).ready(function () {
-  $(".buddy").on("swiperight", function () {
+$(document).ready(function() {
+  $(".buddy").on("swiperight", function() {
     $(this)
       .addClass("rotate-left")
       .delay(700)
@@ -23,7 +23,7 @@ $(document).ready(function () {
     }
   });
 
-  $(".buddy").on("swipeleft", function () {
+  $(".buddy").on("swipeleft", function() {
     $(this)
       .addClass("rotate-right")
       .delay(700)
@@ -53,7 +53,6 @@ const questionSet = {
     ["Food", "Activities", "Career Workshops", "Book cost", "1231"]
   ]
 };
-
 function* questionGen() {
   for (var question in questionSet) {
     yield question;
@@ -62,8 +61,9 @@ function* questionGen() {
 const questionIter = questionGen();
 
 function startQuiz() {
-  list_of_questions = questionSet;
+  // list_of_questions = Object.getOwnPropertyNames(questionSet);
   // for (let question in questionSet) {
+  question = getNextQuestion();
   questionProps = questionSet[question];
   questionType = questionProps[0];
   if (questionType == "T or F") {
@@ -75,11 +75,11 @@ function startQuiz() {
   }
   // }
 }
-
-function showNextQuestion() {
-  valueOfTheQuestionGen = questionIter.next()[
-    (value, isDone)
-  ] = valueOfTheQuestionGen;
+function getNextQuestion() {
+  valueOfTheQuestionGen = questionIter.next();
+  console.log(valueOfTheQuestionGen);
+  value = valueOfTheQuestionGen.value;
+  isDone = valueOfTheQuestionGen.isDone;
   if (isDone) {
     return undefined;
   }
@@ -94,8 +94,38 @@ function clickFunction() {
     console.log(document.getElementsByClassName("pro") + i);
   }
 }
+function showNextQuestion() {
+  question = getNextQuestion();
+  questionProps = questionSet[question];
+  questionType = questionProps[0];
+  questionContainer = document.getElementById("question-container");
+
+  if (questionType == "T or F") {
+    // use the tinder style
+    wrapperDiv = document.createElement("div");
+    wrapperDiv.classList.add("wrapper");
+    questionDiv = document.createElement("div");
+    questionDiv.classList.add("question");
+    questionDiv.innerText = question;
+    agree = document.createElement("sections");
+    disagree = document.createElement("sections");
+
+    agree.innerText = "Agree";
+    disagree.innerText = "Disagree";
+    wrapperDiv.appendChild(questionDiv);
+
+    wrapperDiv.appendChild(agree);
+    wrapperDiv.appendChild(disagree);
+    questionContainer.appendChild(wrapperDiv);
+  } else if (questionType == "Ranking") {
+    // use the ranking style question
+  } else {
+    console.log("Error");
+  }
+}
+
 /*check boxes*/
-$(document).ready(function () {
+$(document).ready(function() {
   var checklistElements = document.querySelectorAll(".ui-checkbox");
   var orders = [];
   SelOrder(checklistElements, orders);
@@ -113,7 +143,7 @@ function SelOrder(checklistElements, orders) {
   for (var i = 0; i < checkboxs.length; i++) {
     checkboxs[i].addEventListener(
       "change",
-      function (e) {
+      function(e) {
         if (e.target.checked) {
           orders.push(e.target.id);
         } else {
@@ -138,7 +168,7 @@ function SelOrder(checklistElements, orders) {
   var clear = document.querySelector(".clr");
   clear.addEventListener(
     "click",
-    function () {
+    function() {
       for (var i = 0; i < checkboxs.length; i++) {
         checkboxs[i].checked = false;
         checkboxLabels[i].classList.remove("ui-checkbox-on");
@@ -150,46 +180,79 @@ function SelOrder(checklistElements, orders) {
 }
 
 // please ignore this code is to make sure the list are sortable on mobile devices
-! function (a) {
+!(function(a) {
   function f(a, b) {
     if (!(a.originalEvent.touches.length > 1)) {
       a.preventDefault();
       var c = a.originalEvent.changedTouches[0],
         d = document.createEvent("MouseEvents");
-      d.initMouseEvent(b, !0, !0, window, 1, c.screenX, c.screenY, c.clientX, c.clientY, !1, !1, !1, !1, 0, null), a.target.dispatchEvent(d)
+      d.initMouseEvent(
+        b,
+        !0,
+        !0,
+        window,
+        1,
+        c.screenX,
+        c.screenY,
+        c.clientX,
+        c.clientY,
+        !1,
+        !1,
+        !1,
+        !1,
+        0,
+        null
+      ),
+        a.target.dispatchEvent(d);
     }
   }
-  if (a.support.touch = "ontouchend" in document, a.support.touch) {
-    var e, b = a.ui.mouse.prototype,
+  if (((a.support.touch = "ontouchend" in document), a.support.touch)) {
+    var e,
+      b = a.ui.mouse.prototype,
       c = b._mouseInit,
       d = b._mouseDestroy;
-    b._touchStart = function (a) {
+    (b._touchStart = function(a) {
       var b = this;
-      !e && b._mouseCapture(a.originalEvent.changedTouches[0]) && (e = !0, b._touchMoved = !1, f(a, "mouseover"), f(a, "mousemove"), f(a, "mousedown"))
-    }, b._touchMove = function (a) {
-      e && (this._touchMoved = !0, f(a, "mousemove"))
-    }, b._touchEnd = function (a) {
-      e && (f(a, "mouseup"), f(a, "mouseout"), this._touchMoved || f(a, "click"), e = !1)
-    }, b._mouseInit = function () {
-      var b = this;
-      b.element.bind({
-        touchstart: a.proxy(b, "_touchStart"),
-        touchmove: a.proxy(b, "_touchMove"),
-        touchend: a.proxy(b, "_touchEnd")
-      }), c.call(b)
-    }, b._mouseDestroy = function () {
-      var b = this;
-      b.element.unbind({
-        touchstart: a.proxy(b, "_touchStart"),
-        touchmove: a.proxy(b, "_touchMove"),
-        touchend: a.proxy(b, "_touchEnd")
-      }), d.call(b)
-    }
+      !e &&
+        b._mouseCapture(a.originalEvent.changedTouches[0]) &&
+        ((e = !0),
+        (b._touchMoved = !1),
+        f(a, "mouseover"),
+        f(a, "mousemove"),
+        f(a, "mousedown"));
+    }),
+      (b._touchMove = function(a) {
+        e && ((this._touchMoved = !0), f(a, "mousemove"));
+      }),
+      (b._touchEnd = function(a) {
+        e &&
+          (f(a, "mouseup"),
+          f(a, "mouseout"),
+          this._touchMoved || f(a, "click"),
+          (e = !1));
+      }),
+      (b._mouseInit = function() {
+        var b = this;
+        b.element.bind({
+          touchstart: a.proxy(b, "_touchStart"),
+          touchmove: a.proxy(b, "_touchMove"),
+          touchend: a.proxy(b, "_touchEnd")
+        }),
+          c.call(b);
+      }),
+      (b._mouseDestroy = function() {
+        var b = this;
+        b.element.unbind({
+          touchstart: a.proxy(b, "_touchStart"),
+          touchmove: a.proxy(b, "_touchMove"),
+          touchend: a.proxy(b, "_touchEnd")
+        }),
+          d.call(b);
+      });
   }
-}(jQuery);
+})(jQuery);
 // SORTABLE
-$(function () {
+$(function() {
   $(".sortable-items").sortable();
   $(".sortable-items").disableSelection();
-
 });
